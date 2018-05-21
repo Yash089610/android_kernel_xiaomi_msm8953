a461d8ae55cd385f524542a2263b7885c41156be
sayan7848 <sayan7848@gmail.com>
2018-05-21 18:51:39 +0000

    Rebrand to horcrux Also add anykernel zip and build script
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

---
+++ mybuild.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+
+CCACHE=$(command -v ccache)
+
+TOOLCHAIN=/home/sayan7848/aarch64-linux-android/bin/aarch64-opt-linux-android-
+
+OUT=$(pwd)/out
+
+TARGET=$OUT/arch/arm64/boot/
+
+JOBCOUNT="-j$(grep -c ^processor /proc/cpuinfo)"
+
+export CROSS_COMPILE="${CCACHE} ${TOOLCHAIN}"
+
+export ARCH=arm64
+
+export KBUILD_BUILD_USER=TheHungarianHorntail
+
+export KBUILD_BUILD_HOST=Butterbeer
+
+echo "Cleaning up"
+make clean O=$OUT
+make mrproper O=$OUT
+
+echo "Preparing kernel config"
+make mido_defconfig O=$OUT
+status=$?
+if [ $status != 0 ]; then
+	echo "You don't have a valid config for your device, code $status"
+	exit $status
+fi
+
+echo "Compiling Kernel"
+make $JOBCOUNT O=$OUT
+status=$?
+if [ $status != 0 ]; then
+	echo "Building interrupted, code $status"
+	exit $status
+fi
+
+echo "Making Flashable Zip"
+ANYKERNEL=$(pwd)/AnyKernel
+BOOT=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+ROOT=$(pwd)
+SUFFIX=$(date +%Y%m%d-%H%M%S)
+cd $ANYKERNEL
+rm -f *.zip
+rm -f zImage
+cp $BOOT $ANYKERNEL
+zip -r9 Horcrux-Treble-$SUFFIX.zip * 
+cd $ROOT
+echo "Flashable zip made, find at AnyKernel2"
+
