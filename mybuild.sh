37917cd342e62782c2c928c12a8559e3c8761f14
sayan7848 <sayan7848@gmail.com>
2018-08-19 21:38:01 +0000

    One script to rule them all Seperate spectrum and non-spectrum dependencies from zipping location Modify build script to handle both spectrum and non-spectrum builds Modify build script to handle both clang and linaro toolchains
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- mybuild.sh
+++ mybuild.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# to run , execute in terminal "mybuild.sh <clean/noclean> <spectrum/nospectrum> <clang/linaro>
 
 CCACHE=$(command -v ccache)
 
-#TOOLCHAIN=/home/sayan7848/aarch64-linux-android/bin/aarch64-opt-linux-android-
+if [ "$3" = "linaro" ]; then
+  TOOLCHAIN=/home/sayan7848/aarch64-linux-android/bin/aarch64-opt-linux-android-
+fi
+
+if [ "$3" = "clang" ]; then
+  TOOLCHAIN=/home/sayan7848/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+fi
 
 OUT=$(pwd)/out
 
@@ -11,21 +18,25 @@ TARGET=$OUT/arch/arm64/boot/
 
 JOBCOUNT="-j$(grep -c ^processor /proc/cpuinfo)"
 
-#export CROSS_COMPILE="${CCACHE} ${TOOLCHAIN}"
-#
-#export ARCH=arm64
+export CROSS_COMPILE="${CCACHE} ${TOOLCHAIN}"
 
 export KBUILD_BUILD_USER=TheHungarianHorntail
 
 export KBUILD_BUILD_HOST=Butterbeer
 
-echo "Cleaning up"
-make clean O=$OUT
-make mrproper O=$OUT
+if [ $3 = "clang" ]; then
+  export CC=/home/sayan7848/linux-x86/clang-r328903/bin/clang
+  export CLANG_TRIPLE=aarch64-linux-gnu-
+fi
+
+if [ "$1" = "clean" ]; then
+  echo "Cleaning up"
+  make clean O=$OUT
+  make mrproper O=$OUT
+fi
 
 echo "Preparing kernel config"
 make O=out ARCH=arm64 mido_defconfig
-#make mido_defconfig O=$OUT
 status=$?
 if [ $status != 0 ]; then
 	echo "You don't have a valid config for your device, code $status"
@@ -33,12 +44,7 @@ if [ $status != 0 ]; then
 fi
 
 echo "Compiling Kernel"
-#make $JOBCOUNT O=$OUT
-make -j$(nproc --all) O=$OUT \
-                      ARCH=arm64 \
-                      CC="/home/sayan7848/linux-x86/clang-r328903/bin/clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="/home/sayan7848/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+make $JOBCOUNT O=$OUT ARCH=arm64
 status=$?
 if [ $status != 0 ]; then
 	echo "Building interrupted, code $status"
@@ -53,8 +59,18 @@ SUFFIX=$(date +%Y%m%d-%H%M%S)
 cd $ANYKERNEL
 rm -f *.zip
 rm -f zImage
+rm -rf ramdisk
+if [ "$2" = "spectrum" ]; then
+  SSUFFIX=Spectrum
+  cp -r $ROOT/spectrum/* $ANYKERNEL/
+fi
+if [ "$2" = "nospectrum" ]; then
+  SSUFFIX=NoSpectrum
+  cp -r $ROOT/nospectrum/* $ANYKERNEL/
+fi
 cp $BOOT $ANYKERNEL
-zip -r9 Horcrux-Treble-$SUFFIX.zip * 
+zip -r9 Horcrux-Treble-$SSUFFIX-$SUFFIX.zip *
+rm -rf ramdisk anykernel.sh
 cd $ROOT
 echo "Flashable zip made, find at AnyKernel2"
 
